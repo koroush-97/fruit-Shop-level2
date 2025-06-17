6ec214d74a3554e45cdd1f991ceea627cf3547b6
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-06-17 09:32:04 +0330

    apiTypes and api config is done

--- src/api/Menu.ts
+++ src/api/Menu.ts
@@ -1,10 +1,10 @@
+import { ApiResponseType, MenuType } from "@/types";
 import apiClient from "./config/ApiClient";
 
-export async function getMenuApiCall() {
-  const response = await apiClient.get("/menus", {
+export async function getMenuApiCall(): Promise<ApiResponseType<MenuType>> {
+  return await apiClient.get("/menus", {
     params: {
       populate: "*",
     },
   });
-  return response.data;
 }

ab1f1a27a4a998f9ad2e1ef3c358702600d5770e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-06-18 16:19:54 +0330

    Category features api has been uploaded

--- src/components/common/image-view/ImageView.tsx
+++ src/components/common/image-view/ImageView.tsx
@@ -1,3 +1,37 @@
+// import Image from "next/image";
+
+// interface Props {
+//   src: string;
+//   alt: string;
+//   width: number;
+//   height: number;
+//   classname?: string;
+// }
+
+// export function ImageView({ classname = "", src, alt, width, height }: Props) {
+//   const isRemote = src.substring(0, 8) === "/uploads";
+//   if (src)
+//     return (
+//       <Image
+//         src={`${isRemote ? "https://nest.navaxcollege.com" + src : src} `}
+//         className={classname}
+//         alt={alt}
+//         width={width}
+//         height={height}
+//       />
+//     );
+//   else {
+//     return (
+//       <Image
+//         src={"/assets/image/Logo.png"}
+//         className={classname}
+//         alt={alt}
+//         width={width}
+//         height={height}
+//       />
+//     );
+//   }
+// }
 import Image from "next/image";
 
 interface Props {
@@ -9,13 +43,26 @@ interface Props {
 }
 
 export function ImageView({ classname = "", src, alt, width, height }: Props) {
-  return (
-    <Image
-      src={src}
-      className={classname}
-      alt={alt}
-      width={width}
-      height={height}
-    />
-  );
+  const isRemote = src.substring(0, 8) === "/uploads";
+  if (src.length > 0)
+    return (
+      <Image
+        src={`${isRemote ? "https://nest.navaxcollege.com" + src : src}`.trim()}
+        className={classname}
+        alt={alt}
+        width={width}
+        height={height}
+      />
+    );
+  else {
+    return (
+      <Image
+        src="/assets/images/Logo.png"
+        className={classname}
+        alt={alt}
+        width={width}
+        height={height}
+      />
+    );
+  }
 }
--- src/components/pages/homepage/featured-categories/FeaturedCategories.tsx
+++ src/components/pages/homepage/featured-categories/FeaturedCategories.tsx
@@ -1,37 +1,51 @@
+import { getFeaturedCategory } from "@/api/Category";
 import { ImageView } from "@/components/common";
 import { featuredCategoriesMock } from "@/mock/featuredCategories";
+import { ApiResponseType, EntityType } from "@/types";
+import { CategoryType } from "@/types/api/Category";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
 interface props {}
 
 export function FeaturedCategories({}: props) {
+  const { data: featureCategory } = useQuery<ApiResponseType<CategoryType>>({
+    queryKey: [getFeaturedCategory.name],
+    queryFn: () => getFeaturedCategory(),
+  });
+
+  console.log("featureCategory", featureCategory);
+
   return (
     <div className="flex flex-wrap justify-between gap-[24px]">
-      {featuredCategoriesMock.map((item, index) => {
-        return (
-          <Link
-            style={{ backgroundImage: item.color }}
-            key={index}
-            href={item.link}
-            className="flex flex-col justify-center items-center text-blue-300 bg-green-100 border-[1px] border-green-100 hover:border-green-300 px-2 lg:px-5 py-3 pt-2 rounded-xl"
-          >
-            <ImageView
-              classname={"mb-2 lg:mb-4"}
-              src={item.img}
-              alt="cat"
-              height={84}
-              width={93}
-            />
+      {featureCategory &&
+        featureCategory.data.map(
+          (item: EntityType<CategoryType>, index: number) => {
+            return (
+              <Link
+                style={{ backgroundImage: item.attributes.color }}
+                key={index}
+                href={item.attributes.link ?? "#"}
+                className="flex flex-col justify-center items-center text-blue-300 bg-green-100 border-[1px] border-green-100 hover:border-green-300 px-2 lg:px-5 py-3 pt-2 rounded-xl"
+              >
+                <ImageView
+                  classname={"mb-2 lg:mb-4"}
+                  src={item.attributes.thumbnail.data?.attributes.url ?? ""}
+                  alt="cat"
+                  height={84}
+                  width={93}
+                />
 
-            <h3 className="text-[12px] text-bold sm:text-heading-sm md:text-heading6 text-center">
-              {item.title}
-            </h3>
-            <span className="text-xsmall text-gray-400 hidden lg:flex">
-              {item.items} items
-            </span>
-          </Link>
-        );
-      })}
+                <h3 className="text-[12px] text-bold sm:text-heading-sm md:text-heading6 text-center">
+                  {item.attributes.title}
+                </h3>
+                <span className="text-xsmall text-gray-400 hidden lg:flex">
+                  {item.attributes.product_count} items
+                </span>
+              </Link>
+            );
+          }
+        )}
     </div>
   );
 }
